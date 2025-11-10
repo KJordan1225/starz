05a798fa065916a6be5003cf89ed9eacfa9e0617
Keith Jordan <shadow902@gmail.com>
2025-11-10 16:29:32 -0500

    update guest home blade view

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('guest.home');
 })->name('guest.home');
 
 Route::get('/dashboard', function () {
